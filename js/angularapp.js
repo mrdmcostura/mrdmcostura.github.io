7c56e5d461921e396798a41505309ee49309bb2c
mrdmcostura <mrdmcostura@gmail.com>
2017-06-15 11:08:05 -0300

    Create angularapp.js

--- js/angularapp.js
+++ js/angularapp.js
@@ -1,126 +1,109 @@
-(function() {
+(function () {
   'use strict';
-  
+
   var baseURL = 'https://mrdmcostura.github.io';
-var app = angular.module("app", ["ngRoute"]); 
-  
-    app.config(function($routeProvider, $locationProvider) {
-      $locationProvider.html5Mode(true);
-$locationProvider.hashPrefix('');
-      $routeProvider
-        .when('/', {
-          templateUrl: baseURL + '/templates/home.html',
-          controller: 'MainController'
-        })
-        .when('/contato', {
-          templateUrl: baseURL + '/templates/contato.html',
-          controller: 'ContatoController'
-        })
-      .when('/hello', {
-          templateUrl: 'hello.html',
-          controller: 'HelloController'
-        })     
-            .when('/empresa', {
-          templateUrl: baseURL + '/templates/empresa.html',
-          controller: 'EmpresaController'
-        })     
-                  .when('/servicos', {
-          templateUrl: baseURL + '/templates/servicos.html',
-          controller: 'ServicosController'
-        })     
-       .otherwise({
+  $('.button-collapse').sideNav();
+  var app = angular.module("app", ["ngRoute"]);
+
+  app.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('');
+    $routeProvider
+      .when('/', {
+        templateUrl: baseURL + '/templates/home.html',
+        controller: 'MainController'
+      })
+      .when('/contato', {
+        templateUrl: baseURL + '/templates/contato.html',
+        controller: 'ContatoController'
+      })
+      .when('/empresa', {
+        templateUrl: baseURL + '/templates/empresa.html',
+        controller: 'EmpresaController'
+      })
+      .when('/servicos', {
+        templateUrl: baseURL + '/templates/servicos.html',
+        controller: 'ServicosController'
+      })
+      .otherwise({
         redirectTo: '/'
-       });
-    
-     // use the HTML5 History API
-      //  $locationProvider.html5Mode(true);
-        //$locationProvider.hashPrefix('');
-
-    
-    });
-  
-//  app.run([ '$templateCache', function($templateCache) {
-//  $templateCache.put('http://www.odati.com.br/templates/home.html', 'http://www.odati.pro/templates/contato.html', 'http://www.odati.com.br/templates/empresa.html', 'http://www.odati.com.br/templates/servicos.html');
-//}]);
-/*  
-  app.run(function($rootScope, $route, $routeParams, $location) {
-      
-      $rootScope.$on('$routeChangeStart',function(evt,next,current){
-        console.log('Nome do Evento:'+evt.name);
-        console.log('Próxima Rota:'+ angular.toJson(next));
-        console.log('Rota Atual:'+ angular.toJson(current));
       });
+  });
+  app.controller("MainController", function ($scope) {
+    $scope.sliders = [{image: "https://mrdmcostura.github.io/images/slider7.jpg", title: "Alta qualidade"},
+                     {image: "https://mrdmcostura.github.io/images/slider2.jpg", title: "Conserto de roupas"},
+                     {image: "https://mrdmcostura.github.io/images/slider6.png", title: "Customização"}];
 
-      $rootScope.$route = $route;
-      $rootScope.$location = $location;
-      $rootScope.$routeParams = $routeParams;
-});
-*/
-  app.controller("MainController", function($scope) {
+    $scope.sections = [{icon: "flash_on", title: "Entrega rápida", body: "Trabalhamos para lhe atender no menor tempo possível."},
+                     {icon: "group", title: "Atenção com as demandas dos clientes", body: "Buscamos que o cliente se sinta realizado em sua individualidade e gosto."},
+                     {icon: "settings", title: "Precisão", body: "Buscamos o melhor acabamento e a melhor qualidade."}];
+   $scope.finish = function(){
     $('.slider').slider();
-});
-      app.controller("NavController", function($scope, $location) {
- $('.button-collapse').sideNav({closeOnClick: true});
-        
-         $scope.isActive = function (viewLocation) { 
-        return viewLocation === $location.path();
+   }
+  });
+  app.controller("NavController", function ($scope, $location) {
+    $('.button-collapse').sideNav({ closeOnClick: true });
+
+    $scope.isActive = function (viewLocation) {
+      return viewLocation === $location.path();
     }
-      });
-  
-  app.controller("HelloController", function($scope) {
 
-});
-  
-    app.controller("FooterController", function($scope) {
+  });
 
-});
-  app.controller("ServicosController", function($scope) {
+  app.controller("FooterController", function ($scope) {
 
-});
-    app.controller("EmpresaController", function($scope) {
+  });
+  app.controller("ServicosController", function ($scope) {
+    $scope.servicos = [{image: "flash_on", title: "Entrega rápida", body: ""},
+                     {image: "group", title: "Trabalhamos para lhe atender no menor tempo possível.", body: ""},
+                     {image: "settings", title: "Customização", body: ""}];
+  });
+  app.controller("EmpresaController", function ($scope) {
 
-});
-    
-      app.directive('navbar', function () {
-  return {
-    restrict: 'A',
-    controller: 'NavController',
-    controllerAs: 'ctrl',
-    templateUrl: baseURL + '/templates/navbar.html'
-  };
-});
-  
-        app.directive('footer', function () {
-  return {
-    restrict: 'A',
-    controller: 'FooterController',
-    controllerAs: 'ctrl',
-    templateUrl: baseURL+ '/templates/footer.html'
-  };
-});
-  
-  app.controller("ContatoController", function($scope, $location) {
-    $scope.mail = {nome: "", email: "", telefone: "", asunto: "", mensagem: ""};
-    $scope.submit = function(event){
-      
-  event.preventDefault();
-    $.ajax({
-    url: "https://formspree.io/mrdmcostura@gmail.com", 
-    method: "POST",
-    data: {message: "Nome: "+ $scope.mail.nome + "\n" +
-                    "Email: "+ $scope.mail.email + "\n" +
-                    "Telefone: "+ $scope.mail.telefone + "\n" +
-                    "Asunto: "+ $scope.mail.asunto + "\n" +
-                    "Mensagem: "+ $scope.mail.mensagem, _replyto: $scope.mail.email},
-    dataType: "json"
-}).done(function(resp){
-      console.log("success");
-$scope.$apply(function(){    
-  $location.path("/");
-    });
-    }).fail(function(error){
-      console.log("fail");
-    });
+  });
+
+  app.directive('navbar', function () {
+    return {
+      restrict: 'A',
+      controller: 'NavController',
+      controllerAs: 'ctrl',
+      templateUrl: baseURL + '/templates/navbar.html'
     };
-    });
+  });
+
+  app.directive('footer', function () {
+    return {
+      restrict: 'A',
+      controller: 'FooterController',
+      controllerAs: 'ctrl',
+      templateUrl: baseURL + '/templates/footer.html'
+    };
+  });
+
+  app.controller("ContatoController", function ($scope, $location) {
+    $scope.mail = { nome: "", email: "", telefone: "", asunto: "", mensagem: "" };
+    $scope.submit = function (event) {
+
+      event.preventDefault();
+      $.ajax({
+        url: "https://formspree.io/mrdmcostura@gmail.com",
+        method: "POST",
+        data: {
+          message: "Nome: " + $scope.mail.nome + "\n" +
+          "Email: " + $scope.mail.email + "\n" +
+          "Telefone: " + $scope.mail.telefone + "\n" +
+          "Asunto: " + $scope.mail.asunto + "\n" +
+          "Mensagem: " + $scope.mail.mensagem, _replyto: $scope.mail.email
+        },
+        dataType: "json"
+      }).done(function (resp) {
+        console.log("success");
+        $scope.$apply(function () {
+          $location.path("/");
+        });
+      }).fail(function (error) {
+        console.log("fail");
+      });
+    };
+  });
 })();
